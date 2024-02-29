c664db3c561d36de7d59ef234729bc1d49f4a5b7
villers <krisztian@villers.hu>
2024-02-29 13:40:36 +0100

    fixing no main monitor error

--- funct/resolution.py
+++ funct/resolution.py
@@ -4,6 +4,8 @@ from screeninfo import get_monitors
 
 # Return the resolution of primary display
 def get_primary():
-    for disp in get_monitors():
-        if disp.is_primary == True:
-            return disp.width, disp.height
+	for disp in get_monitors():
+		if len(get_monitors()) > 1:
+			if disp.is_primary == True:
+				return disp.width, disp.height
+		return disp.width, disp.height
