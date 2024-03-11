affa6d4f0dfbaf292b8d349c2ed807d85ea41a86
Krisztián Villers <krisztian@villers.hu>
2024-03-11 16:52:23 +0100

    Update start.py

--- start.py
+++ start.py
@@ -10,3 +10,6 @@ if __name__ == "__main__":
             MAIN_OPEN, IS_ADMIN = main(admin_mode = False)
         else:
             MAIN_OPEN, IS_ADMIN = main(admin_mode = True)
+            
+# TODO: logolásnál alábbi hiba windowson
+    # UnicodeDecodeError: 'utf-8' codec can't decode byte 0xc1 in position 44: invalid start byte
