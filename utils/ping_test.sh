1e10b0c9469090b9045f056e42954649b975c261
Krisztián Villers <117743062+notvillers@users.noreply.github.com>
2024-04-08 16:53:48 +0200

    new util
    ping_test.sh: pings devices in "script_dir/to_ping.txt"

---
+++ utils/ping_test.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Reads to_ping.txt and returns the result of the ping test to ping_test.log
+
+# Moves to current directory
+script_dir=$(dirname "$0")
+cd $script_dir
+
+log_path="log.txt"
+to_ping="to_ping.txt"
+
+if [ ! -f "$to_ping" ]; then
+    datetime=$(date +"%Y-%m-%d %H:%M:%S")
+    echo "$datetime - file not found: $script_dir/$to_ping"
+    echo "$datetime - file not found: $script_dir/$to_ping" >> $log_path
+    exit 1
+fi
+
+# Read names from file
+names=($(cat $to_ping))
+
+echo "Log path: $script_dir/ping_test.log"
+# Loop through each name in the list
+for name in "${names[@]}"; do
+    # Get the current datetime
+    datetime=$(date +"%Y-%m-%d %H:%M:%S")
+
+    # Ping the name and check the exit status
+    if ping -c 1 "$name" >/dev/null 2>&1; then
+        echo "$datetime - $name is available"
+        echo "$datetime - $name is available" >> $log_path
+    else
+        echo "$datetime - $name is not available"
+        echo "$datetime - $name is not available" >> $log_path
+    fi
+done
