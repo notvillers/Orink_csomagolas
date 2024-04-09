809c49b9de6420dc9610f56739d8ee30619428eb
Krisztián Villers <117743062+notvillers@users.noreply.github.com>
2024-04-09 15:01:14 +0200

    new util (db_save)
    db_save.sh: runs every script file in curr. dir.

---
+++ utils/dbsave/db_save.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Main script that runs all .sh files in the current directory.
+
+# File's dir.
+script_dir=$(dirname "$0")
+cd "$script_dir"
+
+# Log file
+log_file="log.txt"
+
+datetime=$(date +"%Y-%m-%d %H:%M:%S")
+echo "$datetime - script started"
+echo "$datetime - script started" >> $log_file
+
+# Read all .sh files from current directory
+sh_files=$(ls *.sh)
+
+# Loop through each .sh file
+for file in $sh_files; do
+    # Skip the current script file
+    if [ "$file" != "$(basename "$0")" ]; then
+        # Remove the .sh extension
+        file_name=$(basename "$file" .sh)
+        # Print the file name without the .sh extension
+        datetime=$(date +"%Y-%m-%d %H:%M:%S")
+        echo "$datetime - fetching $file_name"
+        echo "$datetime - fetching $file_name" >> $log_file
+        bash $file
+        datetime=$(date +"%Y-%m-%d %H:%M:%S")
+        echo "$datetime - %file_name fetched"
+        echo "$datetime - %file_name fetched" >> $log_file
+fi
+done
+
+datetime=$(date +"%Y-%m-%d %H:%M:%S")
+echo "$datetime - script finished"
+echo "$datetime - script finished" >> $log_file
