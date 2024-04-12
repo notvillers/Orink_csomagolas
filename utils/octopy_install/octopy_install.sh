1562cd6c69553d2aeeefcd4eb605570fb71708c0
Krisztián Villers <117743062+notvillers@users.noreply.github.com>
2024-04-12 13:33:52 +0200

    Update octopy_install.sh

--- utils/octopy_install/octopy_install.sh
+++ utils/octopy_install/octopy_install.sh
@@ -21,24 +21,24 @@ echo "Moving to home directory"
 cd
 
 echo "Removing csomagolas.sh from Desktop"
-rm /Desktop/csomagolas.sh
+rm Desktop/csomagolas.sh
 
 echo "Creating csomagolas.sh on Desktop"
-touch /Desktop/csomagolas.sh
+touch Desktop/csomagolas.sh
 echo "#!/bin/bash" > /Desktop/csomagolas.sh
-echo "sudo bash /home/$username/App/Orink_csomagolas/start_raspi.sh" >> /Desktop/csomagolas.sh
+echo "sudo bash /home/$username/App/Orink_csomagolas/start_raspi.sh" >> Desktop/csomagolas.sh
 
 echo "Changing permissions of csomagolas.sh"
-chmod +x /Desktop/csomagolas.sh
+chmod +x Desktop/csomagolas.sh
 
 echo "Creating autostart directory"
-mkdir /.config/autostart
+mkdir .config/autostart
 
 echo "Removing octopy.desktop from autostart"
-rm /.config/autostart/octopy.desktop
+rm .config/autostart/octopy.desktop
 
 echo "Creating octopy.desktop in autostart"
-touch /.config/autostart/octopy.desktop
+touch .config/autostart/octopy.desktop
 echo "[Desktop Entry]" > /.config/autostart/octopy.desktop
 echo "Type=Application" >> /.config/autostart/octopy.desktop
 echo "Name=Octopy" >> /.config/autostart/octopy.desktop
