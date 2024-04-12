1421ade002e4b938d5cf1ef6314e6df5743e04b0
Krisztián Villers <117743062+notvillers@users.noreply.github.com>
2024-04-12 14:36:12 +0200

    Update octopy_install.sh

--- utils/octopy_install/octopy_install.sh
+++ utils/octopy_install/octopy_install.sh
@@ -1,45 +1,51 @@
 #!/bin/bash
 
+# variables
 username=$(whoami)
+app_dir="App"
+octopy_dir="App/Orink_csomagolas"
+csomagolas_sh="Desktop/csomagolas.sh"
+git_url="https://github.com/notvillers/Orink_csomagolas.git"
+autostart_dir=".config/autostart"
+octopy_desktop=".config/autostart/octopy.desktop"
+
+# script
+echo "Moving to home directory"
+cd
+
+echo "Creating $app_dir directory"
+mkdir "$app_dir"
+
+echo "Removing $octopy_dir"
+sudo rm -r "$octopy_dir"
+
+echo "Cloning $git_url repository"
+cd "$app_dir"
+git clone "$git_url"
 
 echo "Moving to home directory"
 cd
 
-echo "Creating App directory"
-mkdir App
+echo "Removing $csomagolas_sh"
+rm "$csomagolas_sh"
 
-echo "Moving to App directory"
-cd App
+echo "Creating $csomagolas_sh"
+touch "$csomagolas_sh"
+echo "#!/bin/bash" > "$csomagolas_sh"
+echo "sudo bash /home/$username/App/Orink_csomagolas/start_raspi.sh" >> "$csomagolas_sh"
 
-echo "Removing Orink_csomagolas directory"
-sudo rm -r Orink_csomagolas
-
-echo "Cloning Orink_csomagolas repository"
-git clone https://github.com/notvillers/Orink_csomagolas.git
-
-echo "Moving to home directory"
-cd
-
-echo "Removing csomagolas.sh from Desktop"
-rm Desktop/csomagolas.sh
-
-echo "Creating csomagolas.sh on Desktop"
-touch Desktop/csomagolas.sh
-echo "#!/bin/bash" > /Desktop/csomagolas.sh
-echo "sudo bash /home/$username/App/Orink_csomagolas/start_raspi.sh" >> Desktop/csomagolas.sh
-
-echo "Changing permissions of csomagolas.sh"
-chmod +x Desktop/csomagolas.sh
+echo "Changing permissions of $csomagolas_sh"
+chmod +x "$csomagolas_sh"
 
 echo "Creating autostart directory"
-mkdir .config/autostart
+mkdir "$autostart_dir"
 
 echo "Removing octopy.desktop from autostart"
-rm .config/autostart/octopy.desktop
+rm "$octopy_desktop"
 
 echo "Creating octopy.desktop in autostart"
-touch .config/autostart/octopy.desktop
-echo "[Desktop Entry]" > /.config/autostart/octopy.desktop
-echo "Type=Application" >> /.config/autostart/octopy.desktop
-echo "Name=Octopy" >> /.config/autostart/octopy.desktop
-echo "Exec=/home/$username/Desktop/csomagolas.sh" >> /.config/autostart/octopy.desktop
+touch "$octopy_desktop"
+echo "[Desktop Entry]" > "$octopy_desktop"
+echo "Type=Application" >> "$octopy_desktop"
+echo "Name=Octopy" >> "$octopy_desktop"
+echo "Exec=/home/$username/Desktop/csomagolas.sh" >> "$octopy_desktop"
