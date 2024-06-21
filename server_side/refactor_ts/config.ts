dd2edebb91da6c0ba05e2c2242474a4d2e19cf21
Krisztián Villers <117743062+notvillers@users.noreply.github.com>
2024-06-21 14:10:21 +0200

    mods

--- server_side/refactor_ts/config.ts
+++ server_side/refactor_ts/config.ts
@@ -1,12 +1,24 @@
-const path = require('path');
-const fs = require('fs');
+const fs = require('fs'); // File system module
+const path = require('path'); // Path module
 
+// The directory of the script
 export const pathDirectory: string = __dirname;
 
+// The remote directory
 export const remoteDirectory: string = "csomagolas";
 
+// The local directory
 const localDirectory: string = "logs";
 export const localPath: string = path.join(pathDirectory, localDirectory);
+// Create the local directory if it doesn't exist
 if (!fs.existsSync(localPath)) {
     fs.mkdirSync(localPath);
 }
+
+// The FTP file
+const ftpFile: string = "ftp.json";
+export const ftpPath: string = path.join(pathDirectory, ftpFile);
+
+// The login file
+const loginFile: string = "login.json";
+export const loginPath: string = path.join(pathDirectory, loginFile);
--- server_side/refactor_ts/ftp.ts
+++ server_side/refactor_ts/ftp.ts
@@ -2,6 +2,13 @@ import { log } from "console";
 
 const { Client } = require("basic-ftp");
 
+/**
+* Class for handling FTP connections
+* @param server - the FTP server
+* @param user - the FTP user
+* @param password - the FTP password
+* @param debug - whether to print debug messages
+*/
 export class ftp {
     
     server: string;
@@ -16,6 +23,10 @@ export class ftp {
         this.debug = debug;
     }
 
+    /**
+    * @param directory 
+    * @returns the list of files in the directory
+    */
     async listFiles(directory: string = "/") {
         const client = new Client();
         client.ftp.verbose = this.debug;
@@ -28,7 +39,7 @@ export class ftp {
             });
             const filesRaw: any = (await client.list(directory))
             client.close();
-            var files: string[] = [];
+            let files: string[] = [];
             filesRaw.forEach((file: any) => {
                 if (file.type === 1) {
                     files.push(file.name);
@@ -41,6 +52,10 @@ export class ftp {
         client.close();
     }
 
+    /**
+    * @param directory 
+    * @returns the list of directories in the directory
+    */
     async listDirectories(directory: string = "/") {
         const client = new Client();
         client.ftp.verbose = this.debug;
@@ -53,7 +68,7 @@ export class ftp {
             });
             const filesRaw: any = (await client.list(directory))
             client.close();
-            var files: string[] = [];
+            let files: string[] = [];
             filesRaw.forEach((file: any) => {
                 if (file.type === 2) {
                     files.push(file.name);
@@ -66,6 +81,11 @@ export class ftp {
         client.close();
     }
 
+    /**
+    * @param remoteFileName
+    * @param localFileName
+    * Downloads a file from the FTP server
+    */
     async downloadFile(remoteFileName: string, localFileName: string) {
         const client = new Client();
         client.ftp.verbose = this.debug;
@@ -77,12 +97,19 @@ export class ftp {
                 secure: false
             });
             await client.downloadTo(localFileName, remoteFileName);
+            console.log(remoteFileName + " downloaded to " + localFileName);
+            
         } catch(err) {
             console.log(err);
         }
         client.close();
     }
 
+    /**
+    * @param localFileName
+    * @param remoteFileName
+    * Uploads a file to the FTP server
+    */
     async uploadFile(localFileName: string, remoteFileName: string) {
         const client = new Client();
         client.ftp.verbose = true;
